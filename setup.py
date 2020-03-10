cb279921b1b307f16bcf4c36615d38fe7698fe59
Cousin Morgane <morgane.cousin86@gmail.com>
2020-03-10 18:26:14 +0100

    [CHG] modifications into pipfile files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = 'nbodies',
+    name = 'fastphot',
     version = '0.1.0',
     install_requires = ['numpy', 'scipy', 'matplotlib', 'progress'],
     packages = find_packages()
