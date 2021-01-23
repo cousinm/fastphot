bf877c26442d779199a85e68218a19c798cd0804
Cousin Morgane <morgane.cousin86@gmail.com>
2021-01-23 17:22:18 +0100

    [UPD] Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'fastphot',
-    version = '0.1.0',
+    version = '1.1.0',
     install_requires = ['numpy', 'scipy', 'matplotlib', 'progress'],
     packages = find_packages()
 )
